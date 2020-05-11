e605e539ece9bf8de32d9619c6a4b87c86648411
James Heaton <jwheatonumich@gmail.com>
2020-05-11 08:54:10 -0500

    Update JS-Notes-5-loops.js

---
+++ JS-Notes-5-loops.js
@@ -0,0 +1,13 @@
+//While loops will run while a specified condition is true
+var ourArray = [];
+var i = 0;
+while(i < 5) {
+  ourArray.push(i);
+  i++;
+}
+
+//For loop specifies the number of times to run with inputs of initialization, test whether to keep going, and a final expression
+var ourArray = [];
+for (var i = 0; i < 5; i++) {
+  ourArray.push(i);
+}
